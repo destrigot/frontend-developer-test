49388c2571b6bc344a821b6aa57d976a76c05233
nicojones <nicolas.kupfer@mailbox.org>
2021-01-18 12:20:04 +0100

    Ready for Frontend Candidates

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * PLEASE DO NOT CHANGE THIS FILE, AND PERFORM ALL VIEW RENDERING IMPROVEMENTS IN THE PLACES WHERE
+ * IT IS INDICATED.
+ */
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: []
+})
+export class HomeComponent {
+
+  constructor() { }
+}
